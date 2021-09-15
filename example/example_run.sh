1fefc06983a19ea0755ed9f62d4aa6fd5a5c7cde
jnvit-04 <72795530+jnvit-04@users.noreply.github.com>
2021-09-15 17:53:53 +0100

    Update example_run.sh

--- example/example_run.sh
+++ example/example_run.sh
@@ -10,6 +10,6 @@
 # You can explore aditional optional inputs using the flag --help or -h.  Example: pybinde_2021-09.py --help
 
 
-# This next line should work to calculate energies, for a single frame, with a dielectric constant of 4, and saving them to a "energy_file.txt" file.
+# This next line should work to calculate energies, for a single frame, with a dielectric constant of 4, and saving them to a "energy_file.txt" file under the "saved_files" folder.
 python3 ../pybinde_2021-09.py frame00100.gro    1 1149 1150 2298        -ep 4    -sav saved_files/   -ensav saved_files/energy_file.txt
 #         call to program    | gro file      | 2 ranges for objects   | epsin |   pdb saving path  |   energy file path
